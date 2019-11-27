45d73b6c3149fab3c59bc74636f38c7e12bb53d7
jsubhachandra <jsubhachandra@gmail.com>
2019-11-27 21:54:45 +0530

    swagger added

--- src/main/java/com/repayment/plan/PlanGeneratorApplication.java
+++ src/main/java/com/repayment/plan/PlanGeneratorApplication.java
@@ -2,14 +2,23 @@ package com.repayment.plan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+/**
+ * 
+ * @author Subha Chandra
+ *
+ */
 @SpringBootApplication
+@ComponentScan(basePackages="com.repayment")
+@EnableSwagger2
 public class PlanGeneratorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlanGeneratorApplication.class, args);
 	}
 	
-	
-
 }
