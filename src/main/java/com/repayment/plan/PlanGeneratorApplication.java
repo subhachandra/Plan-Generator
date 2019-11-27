8ef6eba471a7ab6328b835ec073bf8fceeae6221
jsubhachandra <jsubhachandra@gmail.com>
2019-11-27 21:34:17 +0530

    swagger added

---
+++ src/main/java/com/repayment/plan/PlanGeneratorApplication.java
@@ -0,0 +1,15 @@
+package com.repayment.plan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanGeneratorApplication.class, args);
+	}
+	
+	
+
+}
