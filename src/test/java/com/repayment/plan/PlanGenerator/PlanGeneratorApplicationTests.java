526899985ae42c5a232734b644b27631da276f00
Subha Chandra <jsubhachandra@gmail.com>
2019-11-28 12:18:34 +0530

    Junit updated
    Junit updated

--- src/test/java/com/repayment/plan/PlanGenerator/PlanGeneratorApplicationTests.java
+++ src/test/java/com/repayment/plan/PlanGenerator/PlanGeneratorApplicationTests.java
@@ -1,13 +1,29 @@
 package com.repayment.plan.PlanGenerator;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+import com.repayment.request.LoanDetails;
+
+@ExtendWith(SpringExtension.class)
 class PlanGeneratorApplicationTests {
 
+	
+	LoanRepaymentServiceImpl loanRepaymentServiceImpl = new LoanRepaymentServiceImpl();
+	
+	/**
+	 * 
+	 */
 	@Test
-	void contextLoads() {
+	public void calculateTest()
+	{
+		LoanDetails loanDetails = new LoanDetails();
+		loanDetails.setLoanAmount(5000l);
+		loanDetails.setDuration(24);
+		loanDetails.setNominalRate(5);
+		loanDetails.setStartDate("2018-01-01T00:00:01Z");
+		loanRepaymentServiceImpl.calculate(loanDetails);
 	}
 
 }
